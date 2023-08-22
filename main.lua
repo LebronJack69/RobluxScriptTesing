a68cdd563660f721a6da7af2ee08a0cc700d4ab5
LebronJack69 <142858396+LebronJack69@users.noreply.github.com>
2023-08-22 05:47:17 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
-local UserInputService = game:GetService("UserInputService")
+--local UserInputService = game:GetService("UserInputService")
 
 local function MakeDraggable(topbarobject, object)
 	local Dragging = nil
@@ -112,7 +112,7 @@ function CreateSupportList(list_game)
 	local UIListLayout = Instance.new("UIListLayout")
 	local CloseButton = Instance.new("TextButton")
 	
-	MakeDraggable(BackImg,BackImg)
+	--MakeDraggable(BackImg,BackImg)
 
 	SupportUi.Name = "SupportUi"
 	SupportUi.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
@@ -198,80 +198,6 @@ function CreateSupportList(list_game)
 		game.Debris:AddItem(SupportUi, 0)
 	end)
 end
-
-local list = {
-	--BedWars
-	[2619619496] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/GAME/main/bedwar.txt'))()",
-
-	--Element Battle Ground
-	[224422602] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/elementBattlegr.lua'))()",
-
-	--King Legacy
-	[1451439645] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/KingLegacyFree.lua'))()",
-
-	--Blox Fruit
-	[994732206] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/BloxFruitFreeV3.lua'))()",
-
-	--Pet Simulator X
-	[2316994223] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/PetSimXFree.lua'))()",
-
-	--Zombie Attack
-	[504035427]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/ZombieAtack.lua'))()",
-
-	--Two Piece
-	[5303541547]="loadstring(game:HttpGet('https://raw.githubusercontent.com/AlongNgu/alonebruh/main/twoPiecehoho.lua'))()",
-
-	--Dungeon Quest
-	[848145103]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/Paid/DunegonQuest.lua'))()",
-
-	--Tradelands
-	[92096286]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/TradeLands.lua'))()",
-
-	--Anime Fighters Simulator
-	[2324662457]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/anime%20fighters%20simulator.lua'))()",
-
-	--Arsenal
-	[111958650]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/Arsenal.lua'))()",
-
-	--Break In
-	[1318971886]="loadstring(game:HttpGet('https://pastebin.com/raw/xUGJt9Ry', true))()",
-
-	--Tower of Hell
-	[703124385]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/TowerOfHell.lua'))()",
-
-	--steve's one piece
-	[741590026]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/SteveOnePiece.lua'))()",
-
-	--Murder Mystery 2
-	[66654135]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/GAME/main/muder%20mys..2.txt'))()",
-
-	--Giant Survival!
-	[1342991001]="loadstring(game:HttpGet('https://pastebin.com/raw/zABEESgr', true))()",
-
-	--Field Trip Z
-	[1701332290]="loadstring(game:HttpGet('https://pastebin.com/raw/aY7Z3Nd3', true))()",
-
-	--Mad City
-	[498490399]="loadstring(game:HttpGet('https://pastebin.com/raw/EwnNCfin', true))()",
-
-	--vn piece
-	[1493595237]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/vn%20Piece%20script-obfuscated.lua', true))()",
-	
-	--Ro Fruit
-	[3904583189]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/RoFruitFree.lua', true))()",
-
-	--Collect All Pets
-	[3359505957]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/CollectAllPetsFree.lua', true))()",
-	
-	--GPO
-	[648454481]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/GPOFree.lua', true))()",
-
-	--Anime Defense Simulator
-	[4205109410]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/AnimeDefenseSimulatorFree.lua', true))()",
-
-	--Anime Soul Simulator
-	[4098816381]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/AnimeSoulSimulatorFree.lua', true))()",
-}
 
 local listPre = {
 	--BedWars
